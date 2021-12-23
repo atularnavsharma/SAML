b1baf06ab8a1f1043cff1658daf716ade400c749
abc <abc@example.com>
2021-12-23 22:29:54 +0530

    Initial commit

---
+++ samlapp/src/main/java/com/test/samlapp/SamlappApplication.java
@@ -0,0 +1,13 @@
+package com.test.samlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlappApplication.class, args);
+	}
+
+}
---
+++ samlapp/src/test/java/com/test/samlapp/SamlappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.samlapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
